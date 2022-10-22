dd4e2ce086069baaad7379e3e4cac413d7256b5c
Hujaifa21 <21hujaifa@gmail.com>
2022-10-23 02:43:41 +0600

    initialized

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import './login.scss';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, error asperiores! Neque itaque impedit</p>
+          <span>Don't you have an acount?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h2>Login</h2>
+          <form action="">
+            <input type="text" placeholder='username'/>
+            <input type="password" placeholder='password'/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
