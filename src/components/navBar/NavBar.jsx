86a6a106e94fa389d27563426cfc095734bad52a
Hujaifa21 <21hujaifa@gmail.com>
2022-10-23 23:48:14 +0600

    navbar completed

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,9 @@
 import './navBar.scss';
 import { Link } from 'react-router-dom';
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineUser, AiOutlineMail } from 'react-icons/ai';
 import { HiOutlineMoon, HiSearch } from 'react-icons/hi';
 import { BiGridAlt } from 'react-icons/bi';
+import { IoMdNotificationsOutline } from 'react-icons/io';
 
 const NavBar = () => {
   return (
@@ -24,7 +25,13 @@ const NavBar = () => {
       </div>
 
       <div className="right">
-
+      <AiOutlineUser/>
+      <AiOutlineMail/>
+      <IoMdNotificationsOutline/>
+      <div className="user">
+        <img src="https://images.pexels.com/photos/4473796/pexels-photo-4473796.jpeg" alt="" />
+        <span>Hujaifa Hossain</span>
+      </div>
       </div>
     </div>
   );
