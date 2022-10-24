dd991f9239d8d9c3b639bc59303d1e6fb628569a
Hujaifa21 <21hujaifa@gmail.com>
2022-10-24 23:54:13 +0600

    rightbar created

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -3,7 +3,28 @@ import './rightBar.scss';
 const RightBar = () => {
   return (
     <div className='rightbar'>
-      right
+      <div className="container">
+        <div className="user-info">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/4473796/pexels-photo-4473796.jpeg" alt="" />
+            <span>Hujaifa Hosssain</span>
+          </div>
+          <div className="buttons">
+            <button>Follow</button>
+            <button>Dismiss</button>
+          </div>
+        </div>
+        <div className="user-info">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/4473796/pexels-photo-4473796.jpeg" alt="" />
+            <span>Hujaifa Hosssain</span>
+          </div>
+          <div className="buttons">
+            <button>Follow</button>
+            <button>Dismiss</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
